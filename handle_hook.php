73ad5f05575d4ec47f3728ad5e252f83e8872980
Ophecy <arthur.de.berranger@gmail.com>
2020-11-19 16:39:37 +0100

    try to fix webhook

--- handle_hook.php
+++ handle_hook.php
@@ -2,9 +2,9 @@
 <?php
 if (session_status() == PHP_SESSION_NONE) session_start();
 // if (count($_POST) == 0) die();
-$json = json_encode($_REQUEST);
+$json = json_encode(file_get_contents('php://input'));
 include_once("inc/db.php");
-$req = $pdo->prepare('INSERT INTO alert SET content=?');
+$req = $pdo->prepare('INSERT INTO alert SET content=?, request=?');
 $req->execute([$json]);
 var_dump($json);
 

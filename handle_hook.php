7d3c2ab8a306f773fcb7401a9d8272a3647e8782
Ophecy <arthur.de.berranger@gmail.com>
2020-11-19 17:15:56 +0100

    finally fixed hook

--- handle_hook.php
+++ handle_hook.php
@@ -1,8 +1,8 @@
 <pre>
 <?php
-// if (session_status() == PHP_SESSION_NONE) session_start();
-// if (count($_POST) == 0) die();
 $json = json_encode(file_get_contents('php://input'));
+if (strlen($json) < 10) die();
+
 include_once("inc/db.php");
 $req = $pdo->prepare('INSERT INTO alert SET content=?');
 $req->execute([$json]);

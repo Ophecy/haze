e50ee1262ff1e8b4554c34b1fe21d20956e92536
Ophecy <arthur.de.berranger@gmail.com>
2020-11-19 15:41:27 +0100

    test webhook

--- handle_hook.php
+++ handle_hook.php
@@ -1,11 +1,12 @@
 <pre>
 <?php
-if (count($_POST) == 0) die();
+// if (count($_POST) == 0) die();
 $post = json_encode($_POST);
 include_once("inc/db.php");
 $req = $pdo->prepare('INSERT INTO alert SET content=?');
-
 $req->execute([$post]);
+
+var_dump($post);
 
 ?>
 </pre>
